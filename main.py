5dc9c5c2acbc40b6e7e66948aae65740de58ee61
barturba <b@bartas.co>
2024-07-16 15:11:30 -0600

    feat: Refactor main.py to use a class for window management

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-from tkinter import Tk, BOTH, Canvas
-
-
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title = "Maze Solver"
-        self.__canvas = Canvas()
-        self.__canvas.pack()
-        self.running = False
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.running = True
-        while self.running:
-            self.redraw()
-
-    def close(self):
-        self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+from graphics import Window
 
 
 def main():
