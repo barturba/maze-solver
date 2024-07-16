c210826c6f396ea3eefbf4f68d54b88f753f566c
barturba <b@bartas.co>
2024-07-16 15:50:09 -0600

    Refactor graphics.py to add draw_line method

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from graphics import Window
+from graphics import Point, Line, Window
 
 
 def main():
     win = Window(800, 600)
     print("started window")
+    win.draw_line(Line(Point(0, 0), Point(800, 600)), "green")
+    win.draw_line(Line(Point(400, 250), Point(600, 250)), "red")
     win.wait_for_close()
 
 
