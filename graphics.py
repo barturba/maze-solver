44de284e4cc87db61c5c781bddb69beae0d8bf8a
barturba <b@bartas.co>
2024-07-19 10:47:00 -0600

    Refactor main.py to use Cell class for drawing and moving cells

--- graphics.py
+++ graphics.py
@@ -42,3 +42,51 @@ class Line:
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
+
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+
+        top_left = Point(self._x1, self._y1)
+        top_right = Point(self._x2, self._y1)
+        bottom_left = Point(self._x1, self._y2)
+        bottom_right = Point(self._x2, self._y2)
+        left = Line(top_left, bottom_left)
+        right = Line(top_right, bottom_right)
+        top = Line(top_left, top_right)
+        bottom = Line(bottom_left, bottom_right)
+        if self.has_left_wall:
+            self._win.draw_line(left, "red")
+        if self.has_right_wall:
+            self._win.draw_line(right, "red")
+        if self.has_top_wall:
+            self._win.draw_line(top, "red")
+        if self.has_bottom_wall:
+            self._win.draw_line(bottom, "red")
+
+    def draw_move(self, to_cell, undo=False):
+        color = "gray"
+        if not undo:
+            color = "red"
+        center_x = self._x1 - ((self._x1 - self._x2) / 2)
+        center_y = self._y1 - ((self._y1 - self._y2) / 2)
+
+        other_center_x = to_cell._x1 - ((to_cell._x1 - to_cell._x2) / 2)
+        other_center_y = to_cell._y1 - ((to_cell._y1 - to_cell._y2) / 2)
+        line = Line(Point(center_x, center_y), Point(other_center_x, other_center_y))
+        self._win.draw_line(line, color)
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from graphics import Point, Line, Window
+from graphics import Cell, Point, Line, Window
 
 
 def main():
     win = Window(800, 600)
     print("started window")
-    win.draw_line(Line(Point(0, 0), Point(800, 600)), "green")
-    win.draw_line(Line(Point(400, 250), Point(600, 250)), "red")
+    c = Cell(win)
+    d = Cell(win)
+
+    d.draw(50, 50, 100, 100)
+
+    c.draw(300, 300, 500, 500)
+    c.draw_move(d, True)
+
     win.wait_for_close()
 
 
