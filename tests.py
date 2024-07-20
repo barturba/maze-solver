f16cdd6cd50eaeb8bc246850f01db820f27934a5
barturba <b@bartas.co>
2024-07-20 17:21:25 -0600

    Refactor test_maze_break_entrance_and_exit test in tests.py

--- tests.py
+++ tests.py
@@ -33,12 +33,12 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
-    # def test_maze_break_entrance_and_exit(self):
-    #     num_cols = 2
-    #     num_rows = 9
-    #     m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
-    #     self.assertEqual(m1._cells[0][0].has_top_wall, False)
-    #     self.assertEqual(m1._cells[num_cols - 1][num_rows - 1].has_right_wall, False)
+    def test_maze_break_entrance_and_exit(self):
+        num_rows = 2
+        num_cols = 9
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, None)
+        self.assertEqual(m1._cells[0][0].has_top_wall, False)
+        self.assertEqual(m1._cells[num_cols - 1][num_rows - 1].has_right_wall, False)
 
 
 if __name__ == "__main__":
